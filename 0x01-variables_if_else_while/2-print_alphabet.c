f8d2e500f29bb6e2c33593670da929b7fb8b1c1b
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-09 21:14:05 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - prints out the alphabet
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
