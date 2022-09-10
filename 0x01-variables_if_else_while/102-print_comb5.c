1e5e6055b7967ad7a815bf2b3ee5276bd3989f7c
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-10 07:07:46 -0700

    files

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two digit numbers
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	int dig1;
+	int dig2;
+
+	for (dig1 = 0; dig1 < 98; dig1++)
+	{
+		for (dig2 = dig1 + 1; dig2 < 99; dig2++)
+		{
+			putchar((dig1 / 10) + '0');
+			putchar((dig1 % 10) + '0');
+			putchar(' ');
+			putchar((dig2 / 10) + '0');
+			putchar((dig2 % 10) + '0');
+		
+			if (dig1 == 98 && dig2 == 99)
+			{
+				continue;
+			}
+			putchar(',');
+		}	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
