dd484180c645f3b632e5327345760ecaa00e69cf
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-13 13:12:38 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,6 +23,12 @@ void print_times_table(int n)
 				if (z <= 9)
 				{
 					_putchar(32);
+					_putchar(32);
+					_putchar(z + 48);
+				}
+				else if (z <= 99)
+				{
+					_putchar(32);
 					_putchar((z / 10) + 48);
 					_putchar((z % 10) + 48);
 				}
