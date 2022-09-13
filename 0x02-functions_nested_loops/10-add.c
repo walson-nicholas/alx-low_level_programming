e3fd0c998a973d01292261277498d98bab48dc3a
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-13 11:59:21 -0700

    addition function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - funtion to add two numbers
+ * @a, b: input numbers to be added
+ * Return: 0
+ */
+
+int add (int a, int b)
+{
+	int c;
+	c = a + b;
+	return (c);
+}
+	
