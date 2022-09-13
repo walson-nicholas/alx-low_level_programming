152a2150131cbe148db9f426068ca2689ae15011
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-13 10:02:58 -0700

    return absolute value to input variable

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - return the absolute value to a number
+ * @c: input variable
+ * Return: 0
+ */
+
+int _abs(int c)
+{
+	if(c <= 0)
+	{
+		return (c * -1)
+	}
+	else
+	{
+		return (c);
+	}
+}
