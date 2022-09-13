7e3394cf882f28e830e93caf941ca39ee5343b3a
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-13 12:40:15 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints to 98 either forward or backward
+ * @n: input integer
+ * Return: print out of number to 98
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98, n++)
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	else if (n > 98, n--)
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	_putchar(98);
+}
