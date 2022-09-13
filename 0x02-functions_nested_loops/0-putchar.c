438a2db61850810caa9478dc1bd805b2a8515d7a
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-13 07:14:05 -0700

    print putchar and add main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints a set of characters
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	char x[] = "_putchar";
+	int i = 0;
+
+	while (x[i] != '\0')
+	{
+		_putchar(x[i]);
+		i++;
+	}
+	_putchar(10);
+	
+	return(0);
+}
