19b51e1512e53815c21467c3b658ac7dce0e8001
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-13 12:44:49 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,16 @@
 void print_to_98(int n)
 {
 	if (n < 98; n++)
+	{
 		_putchar(n);
 		_putchar(',');
 		_putchar(' ');
+	}
 	else if (n > 98; n--)
+	{
 		_putchar(n);
 		_putchar(',');
 		_putchar(' ');
+	}
 	_putchar(98);
 }
