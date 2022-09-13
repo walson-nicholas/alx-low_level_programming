893d9fc080299d7a86822a4754211aafa6edb24a
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-13 07:53:43 -0700

    check is alphabet is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks if alphabet is lowercase
+ * Return: 1 for success,  0 for failure
+ */
+
+int _islower(int c)
+{
+	if (l >= 97 && l <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
