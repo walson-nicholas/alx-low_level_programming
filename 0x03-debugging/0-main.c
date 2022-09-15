e710cf0581422b2e41fe24273d2ecd43119bd332
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-14 21:42:47 -0700

    debug positive or negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - function that runs other functions
+ * Return: void
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
