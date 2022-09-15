110fbe6937522d147e7bd5fbfba1ec8fa6128526
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-15 01:16:44 -0700

    check if input is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if input is digit from 1 to 9
+ * @c: input number
+ * Return: 1 if digit, 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
