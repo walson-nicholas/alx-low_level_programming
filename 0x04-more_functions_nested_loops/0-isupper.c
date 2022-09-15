887dc6740ca60cb0c5e944cc5a578dffcc876a9a
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-15 00:34:49 -0700

    readme and uppercase function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: input letter
+ * Return: 1 if uppercase, 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
