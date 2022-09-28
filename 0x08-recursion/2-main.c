05a3a5c05b9f4079aac695a12bbc75205d4f376d
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-28 12:19:30 -0700

    determines the length of a string

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ * Return - always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Asimiea Nicholas Walson");
+	printf("%d\n", n);
+	return (0);
+}
