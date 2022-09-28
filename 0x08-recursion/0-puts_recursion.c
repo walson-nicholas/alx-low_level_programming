db9687f9ab974190f0b89eadd3a8c3f960303e3b
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-28 11:00:49 -0700

    prints characters of a string recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - main
+ * @s: var
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+	
+	for (i = 0; *(s + i) != '\0'; i++)
+		_putchar(*(s + i));
+	_putchar('\n');
+}
