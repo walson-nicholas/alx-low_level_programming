87dfc41ee7ee69c0cdb241c0c957fa37cfc1ffad
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-09-30 15:51:53 -0700

    adds arguments to function

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - func
+ * @argc: var 1
+ * @argv: var 2
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x = 0, i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			x += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", x);
+	return (0);
+}
