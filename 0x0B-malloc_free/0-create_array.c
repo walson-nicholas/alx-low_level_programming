650d8c7a23db292479c77154594cbbdde89dfc5c
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-10-04 03:43:02 -0700

    create array and returns pointer to array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * create_array - create and initialize an array of characters
+ * @size: lenght of the array
+ * @c: the character used to initialize the array
+ * Return: pointer to to the creater array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *x;
+	unsigned int i;
+
+	x = (char *)malloc(size * sizeof(char));
+	if (x == NULL)
+	{
+		printf("ERROR");
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		x[i] = c;
+	}
+	return (x);
+}
