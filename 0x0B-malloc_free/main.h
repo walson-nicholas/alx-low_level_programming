3dd216bcd9195c8410e1090c79381b58c7af24de
Asimiea Nicholas <asimieanicholas@gmail.com>
2022-10-04 05:55:31 -0700

    update header file containing function prototypes

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include "stdio.h"
+#include "stdlib.h"
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
